7494d60a46dc151b287abc77137ab637264e4e96
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-11-16 19:56:03 -0500

    Add ActorClient integration to AcledClient
    This commit introduces the ActorClient to the AcledClient class, ensuring it can now retrieve actor-specific data through the new `get_actor_data` property. It resolves a minor bug where the actor client was incorrectly instantiated as `AcledClient`.

--- acled/clients/client.py
+++ acled/clients/client.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from .acled_data_client import AcledDataClient
+from .actor_client import ActorClient
 
 
 class AcledClient:
@@ -16,10 +17,13 @@ class AcledClient:
         self.api_key = api_key
         self.email = email
         self.acled_data_client = AcledDataClient(api_key, email)
-        self.actor_client = AcledClient(api_key, email)
+        self.actor_client = ActorClient(api_key, email)
 
 
     @property
     def get_data(self):
         return self.acled_data_client.get_data
 
+    @property
+    def get_actor_data(self):
+        return self.actor_client.get_data
