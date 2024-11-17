1c7b7aebb2b7e56597e00173a80811cbe0ea14e7
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-11-16 21:28:10 -0500

    Add new clients and properties to AcledClient
    Integrated ActorTypeClient, CountryClient, and RegionClient into AcledClient. New properties get_actor_type_data, get_country_data, and get_region_data added to facilitate data retrieval from these clients.

--- acled/clients/client.py
+++ acled/clients/client.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from .acled_data_client import AcledDataClient
 from .actor_client import ActorClient
+from .actor_type_client import ActorTypeClient
+from .country_client import CountryClient
+from .region_client import RegionClient
 
 
 class AcledClient:
@@ -18,6 +21,9 @@ class AcledClient:
         self.email = email
         self.acled_data_client = AcledDataClient(api_key, email)
         self.actor_client = ActorClient(api_key, email)
+        self.country_client = CountryClient(api_key, email)
+        self.region_client = RegionClient(api_key, email)
+        self.actor_type_client = ActorTypeClient(api_key, email)
 
 
     @property
@@ -27,3 +33,15 @@ class AcledClient:
     @property
     def get_actor_data(self):
         return self.actor_client.get_data
+
+    @property
+    def get_actor_type_data(self):
+        return self.actor_type_client.get_data
+
+    @property
+    def get_country_data(self):
+        return self.country_client.get_data
+
+    @property
+    def get_region_data(self):
+        return self.region_client.get_data
